a0ae8e68e537d37c017d2f705241dc56060a587b
debfdias <debfdias@gmail.com>
2023-08-30 08:41:06 -0300

    Fix unescaped entitie

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <p className="mb-3 text-4xl font-semibold text-pink-500">Hi, I'm Debs</p>
-      <p>This is a work in progress... wait for it, it's gonna be...</p>
+      <p className="mb-3 text-4xl font-semibold text-pink-500">
+        Hi, I&apos;m Debs
+      </p>
+      <p>This is a work in progress... wait for it, it&apos;s gonna be...</p>
       <h1 className="pt-[30px] font-bold text-4xl">Legendary!</h1>
     </main>
   )
