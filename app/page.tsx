fcb868cfaefa9cc59395214eb8b8fcbf3645efc4
debfdias <debora.fortunato.-nd@disney.com>
2023-08-30 08:37:13 -0300

    Favicon, colors, header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className={`mb-3 text-2xl font-semibold`}>debs.dev</h2>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <p className="mb-3 text-4xl font-semibold text-pink-500">Hi, I'm Debs</p>
+      <p>This is a work in progress... wait for it, it's gonna be...</p>
+      <h1 className="pt-[30px] font-bold text-4xl">Legendary!</h1>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        pink: {
+          500: "#e95373",
+        },
+        blue: {
+          500: "#272940",
+          600: "#1c1e30",
+          700: "#141527",
+        },
+        gray: {
+          100: "#eeeef0",
+          200: "#bbbfcf",
+        },
       },
     },
   },
