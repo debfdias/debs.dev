3f9cae87e899a155a4a1a1a5eee8d6106f55a52f
debfdias <debfdias@gmail.com>
2023-09-07 23:18:18 -0300

    Send message flow

--- components/Form/index.tsx
+++ components/Form/index.tsx
@@ -1,34 +1,83 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 import { LuSend } from "react-icons/lu"
+import z from "zod"
+import InputText from "../Input"
+
+const formSchema = z.object({
+  name: z
+    .string({
+      required_error: "Fill this field.",
+      invalid_type_error: "Name must be a string.",
+    })
+    .min(6, { message: "Conta contrato possui 6 dígitos." }),
+  email: z.string().email({ message: "Invalid e-mail." }),
+  message: z.string().min(10),
+})
+
+type FormData = z.infer<typeof formSchema>
 
 export default function ContactForm() {
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  })
+
+  async function handleSendMessage(data: any) {
+    console.log(data)
+    reset()
+  }
+
   return (
     <div className="sm:px-12 sm:pr-6 pb-12 pr-4">
-      <label className="font-thin text-sm text-gray-300 mb-2 flex">Name</label>
-      <input
-        className="bg-blue-500 p-2 rounded-md text-gray-300 w-full mb-6 text-md font-thin focus:outline-none focus:ring-1 focus:ring-pink-500 "
-        placeholder="John Doe"
-      ></input>
+      <form action="" method="POST" onSubmit={handleSubmit(handleSendMessage)}>
+        <label className="font-thin text-sm text-gray-300 mb-2 flex">
+          Name
+        </label>
+        <InputText
+          label="John Doe"
+          name="name"
+          type="text"
+          error={errors?.name?.message}
+          register={register}
+        />
 
-      <label className="font-thin text-sm text-gray-300 mb-2 flex">
-        E-mail
-      </label>
-      <input
-        className="bg-blue-500 p-2 rounded-md text-gray-300 w-full mb-6 font-thin focus:outline-none focus:ring-1 focus:ring-pink-500 "
-        placeholder="your@email.com"
-      ></input>
+        <label className="font-thin text-sm text-gray-300 mb-2 flex">
+          E-mail
+        </label>
+        <InputText
+          label="your@email.com"
+          name="email"
+          type="text"
+          error={errors?.email?.message}
+          register={register}
+        />
 
-      <label className="font-thin text-sm text-gray-300 mb-2 flex">
-        Message
-      </label>
-      <textarea
-        className="bg-blue-500 p-2 rounded-md text-gray-300 w-full mb-6 font-thin focus:outline-none focus:ring-1 focus:ring-pink-500 resize-none h-[120px]"
-        placeholder="Your awesome message"
-      ></textarea>
+        <label className="font-thin text-sm text-gray-300 mb-2 flex">
+          Message
+        </label>
+        <textarea
+          {...register("message")}
+          name="message"
+          id="message"
+          className="bg-blue-500 p-2 rounded-md text-gray-300 w-full mb-6 font-thin focus:outline-none focus:ring-1 focus:ring-pink-500 resize-none h-[120px]"
+          placeholder="Your awesome message"
+        ></textarea>
 
-      <button className="flex text-gray-200 items-center gap-2 hover:text-pink-500 hover:scale-[1.05] hover:translate-x-1 transition ease-in-out delay-150">
-        <LuSend size={24} />
-        <div className="font-semibold whitespace-nowrap text-lg">Send it</div>
-      </button>
+        <button
+          type="submit"
+          className="flex text-gray-200 items-center gap-2 hover:text-pink-500 hover:scale-[1.05] hover:translate-x-1 transition ease-in-out delay-150"
+        >
+          <LuSend size={24} />
+          <div className="font-semibold whitespace-nowrap text-lg">Send it</div>
+        </button>
+      </form>
     </div>
   )
 }
---
+++ components/Input/index.tsx
@@ -0,0 +1,17 @@
+const InputText = ({ label, name, type, register, error }: any) => {
+  return (
+    <>
+      <input
+        id={name}
+        name={name}
+        type={type}
+        {...register(name)}
+        placeholder={label}
+        className="bg-blue-500 p-2 rounded-md text-gray-300 w-full mb-6 text-md font-thin focus:outline-none focus:ring-1 focus:ring-pink-500 "
+      />
+      <p className="mb-2 text-xs text-red-500 font-medium">{error}</p>
+    </>
+  )
+}
+
+export default InputText
