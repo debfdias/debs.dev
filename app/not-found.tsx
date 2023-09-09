9a9cc77c64414862ca58bc45b9fc69b9d4aec81c
debfdias <debfdias@gmail.com>
2023-09-09 17:23:33 -0300

    Style not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,25 @@
 "use client"
+import AnimatedByWord from "@/components/AnimatedByWord"
+import { motion } from "framer-motion"
 import { LiaMapSignsSolid } from "react-icons/lia"
 
 export default function NotFound() {
   return (
     <div className="">
-      <div className="text-2xl sm:text-4xl flex items-center gap-8">
-        <LiaMapSignsSolid size={36} />
-        Nothig found here. Are you lost?
+      <div className="text-2xl sm:text-4xl flex gap-8 items-end">
+        <motion.button
+          animate={{ opacity: 1, rotate: [0, 0, 25, -25, 0] }}
+          transition={{
+            duration: 5,
+            ease: "backInOut",
+            times: [0, 0.2, 0.5, 0.8, 1],
+            repeat: Infinity,
+            repeatDelay: 1,
+          }}
+        >
+          <LiaMapSignsSolid size={64} />
+        </motion.button>
+        <AnimatedByWord text="Nothig found here. Are you lost?" style="mb-1" />
       </div>
     </div>
   )
